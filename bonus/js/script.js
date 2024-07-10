b8f66ecd7d8df8f6374bbae9e7c68333d836c910
andreacagnina <amirkal@libero.it>
2024-07-10 16:43:14 +0200

    [feat] created row e col func

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,7 +1,30 @@
 const container = document.getElementById('container');
-container.classList.add('text-center', 'my-5');
-container.innerHTML = `<h1>ciao</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur repellendus dolorem nam! Culpa vitae enim labore tenetur odio ipsam iusto id praesentium sint iure facere, qui, excepturi doloremque, accusantium voluptatem?</p>`
+container.classList.add('container', 'text-center', 'my-5');
+container.innerHTML = `<h1>OUR TEAM</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur repellendus dolorem nam! Culpa vitae enim labore tenetur odio ipsam iusto id praesentium sint iure facere, qui, excepturi doloremque, accusantium voluptatem?</p>`
 
+function createRow() {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    return row
+}
+
+function createCol() {
+    let col = document.createElement('div');
+    col.classList.add('col-12', 'col-md-6', 'col-lg-4');
+    return col
+}
+
+let newRow = createRow();
+let newCol = createCol();
+
+container.append(newRow);
+newRow.append(newCol);
+
+
+for (i = 0; i < members.length; i++) {
+    console.log(members[i].nome, members[i].ruolo, members[i].foto)
+    text.innerHTML += `${members[i].nome}, ${members[i].ruolo}, <img src="../img/${members[i].foto}"> <br>`
+}
 
 let members = [
     {
@@ -35,8 +58,3 @@ let members = [
         foto: 'barbara-ramos-graphic-designer.jpg'
     }
 ];
-
-for (i = 0; i < members.length; i++) {
-    console.log(members[i].nome, members[i].ruolo, members[i].foto)
-    text.innerHTML += `${members[i].nome}, ${members[i].ruolo}, <img src="../img/${members[i].foto}"> <br>`
-}
