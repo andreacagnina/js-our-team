7e6e894190586b03abf18a113c50cfaab79513b0
andreacagnina <amirkal@libero.it>
2024-07-10 17:18:49 +0200

    [feat] completed bonus 2

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -32,8 +32,9 @@ let members = [
 ];
 
 const container = document.getElementById('container');
-container.classList.add('container', 'text-center', 'my-5');
+container.classList.add('container-fluid', 'text-center', 'py-5', 'bg-light');
 container.innerHTML = `<h1>OUR TEAM</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur repellendus dolorem nam! Culpa vitae enim labore tenetur odio ipsam iusto id praesentium sint iure facere, qui, excepturi doloremque, accusantium voluptatem?</p>`
+
 
 function createRow() {
     let row = document.createElement('div');
@@ -49,8 +50,14 @@ function createCol() {
 
 function createCont() {
     let cont = document.createElement('div');
-    cont.classList.add('m-2');
+    cont.classList.add('content', 'my-3');
     return cont
+}
+
+function createCard() {
+    let card = document.createElement('div');
+    card.classList.add('card', 'h-100');
+    return card
 }
 
 let newRow = createRow();
@@ -61,8 +68,10 @@ container.append(newRow);
 for (i = 0; i < members.length; i++) {
     let newCol = createCol();
     let newCont = createCont();
+    let newCard = createCard();
     console.log(members[i].nome, members[i].ruolo, members[i].foto)
-    newCont.innerHTML += `${members[i].nome}, ${members[i].ruolo}, <img src="../img/${members[i].foto}"> <br>`
+    newCard.innerHTML += `<img src="../img/${members[i].foto}"><div class="card-body"><h5 class="card-title">${members[i].nome}</h5> <p class="card-text fst-italic">${members[i].ruolo}</p></div>`
     newRow.append(newCol);
     newCol.append(newCont);
+    newCont.append(newCard);
 }
